db8e5113d94ab9a3de54fb54087990c24a77d4b7
aleks@amd <alkyl1978@yandex.ru>
2017-05-28 12:29:22 +0300

    init

---
+++ kodi.yarnet/core/defines.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+SITE_DOMAIN = 'yar-net.ru'
+SITE_URL = 'http://'+SITE_DOMAIN
+PLUGIN_ID = 'kodi.yarnet'
+CACHE_DATABASE = 'yarnet.cache.db'
+COOKIE_FILE = 'yarnet_cookie.txt'
+
+QUALITYS = [None, '360', '480', '720', '1080']
