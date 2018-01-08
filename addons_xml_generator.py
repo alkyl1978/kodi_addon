684dc1be2fdd4ecd5101c83a2c5b37381cdf1e34
aleks <amd.lan>
2018-01-08 17:34:41 +0300

    repo edit

--- addons_xml_generator.py
+++ addons_xml_generator.py
@@ -2,6 +2,8 @@
 
 import os
 import md5
+import re
+from shutil import make_archive,copyfile
 
 
 class Generator:
@@ -52,6 +54,7 @@ class Generator:
         # save file
         self._save_file( addons_xml.encode( "UTF-8" ), file="addons.xml" )
 
+
     def _generate_md5_file( self ):
         try:
             # create a new md5 hash
@@ -70,6 +73,17 @@ class Generator:
             # oops
             print "An error occurred saving %s file!\n%s" % ( file, e, )
 
+    def make_zipfile(output_filename, source_dir):
+        relroot = os.path.abspath(os.path.join(source_dir, os.pardir))
+        with zipfile.ZipFile(output_filename, "w", zipfile.ZIP_DEFLATED) as zip:
+            for root, dirs, files in os.walk(source_dir):
+                # add directory (needed for empty dirs)
+                zip.write(root, os.path.relpath(root, relroot))
+                for file in files:
+                    filename = os.path.join(root, file)
+                    if os.path.isfile(filename):  # regular files only
+                        arcname = os.path.join(os.path.relpath(root, relroot), file)
+                        zip.write(filename, arcname)
 
 if ( __name__ == "__main__" ):
     # start
